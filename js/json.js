354ebddd668b688e11d1037f7ccd85a9b415d1d8
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2022-08-06 06:11:51 -0400

    add final section JSON

---
+++ js/json.js
@@ -0,0 +1,38 @@
+/* **********     Curso JavaScript: 59. JSON      ********** */
+/* https://www.json.org/json-es.html */
+/* https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/JSON */
+
+/* const jean = {
+  cadena: "Jean Carlos",
+  numero: 26,
+  booleano: true,
+  arreglo: ["Leer", "One Piece", "Aprender"],
+  objeto: {
+    twitter: "iFerre_11",
+    email: ["jeancarlostrejo19@gmail.com", "jeancarlos.trejo@unet.edu.ve"],
+  },
+  nulo: null,
+};
+
+console.log(JSON);
+//El método JSON.parse() analiza una cadena de texto como JSON, transformando opcionalmente el valor producido por el análisis.
+console.log(JSON.parse("{}"));
+console.log(JSON.parse("[1,2,3]"));
+console.log(JSON.parse("true"));
+console.log(JSON.parse("false"));
+console.log(JSON.parse("19"));
+// console.log(JSON.parse('"Hola Mundo"'));
+console.log(JSON.parse("null"));
+console.log(
+  JSON.parse(
+    '{"cadena": "Jean Carlos","numero": 26,"booleano": true,"arreglo": ["Leer", "One Piece", "Aprender"],"objeto": {"twitter": "iFerre_11","email": ["jeancarlostrejo19@gmail.com", "jeancarlos.trejo@unet.edu.ve"]},"nulo": null}'
+  )
+);
+
+//El método JSON.stringify() convierte un objeto o valor de JavaScript en una cadena de texto JSON, opcionalmente reemplaza valores si se indica una función de reemplazo, o si se especifican las propiedades mediante un array de reemplazo.
+console.log(JSON.stringify({}));
+console.log(JSON.stringify([1, 2, 3]));
+console.log(JSON.stringify(true));
+console.log(JSON.stringify(19));
+console.log(JSON.stringify({ x: 2, y: 3 }));
+console.log(JSON.stringify(jean)); */
